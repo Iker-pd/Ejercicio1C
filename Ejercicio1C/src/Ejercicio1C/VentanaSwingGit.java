a97f45acf2f61343e4298b9963775a12898ce740
Iker-pd <iker.perez.dulce@opendeusto.es>
2025-09-29 09:40:55 +0200

    Primeras Modificaciones

--- Ejercicio1C/src/Ejercicio1C/VentanaSwingGit.java
+++ Ejercicio1C/src/Ejercicio1C/VentanaSwingGit.java
@@ -1,12 +1,51 @@
 package Ejercicio1C;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class VentanaSwingGit extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+	
+	public VentanaSwingGit() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Ejemplo Swing + GIT");
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+		
+		JMenuBar barraMenu = new JMenuBar();
+		setJMenuBar(barraMenu);
+		
+		JMenu menuFichero = new JMenu("Fichero");
+		barraMenu.add(menuFichero);
+		
+		JMenuItem itemMostrar = new JMenuItem("Mostrar");
+		menuFichero.add(itemMostrar);
+		JMenuItem itemLimpiar = new JMenuItem("Limpiar");
+		menuFichero.add(itemLimpiar);
+		JMenuItem itemSalir = new JMenuItem("Salir");
+		menuFichero.add(itemSalir);
+
+
+		
+		JTabbedPane panelPestanas = new JTabbedPane();
+		add(panelPestanas, BorderLayout.CENTER);
+		
+		panelPestanas.addTab("Datos Personales", new JPanel());
+		panelPestanas.add("Informacion", new JPanel());
+		panelPestanas.add("Comentarios", new JPanel());
+		
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		new VentanaSwingGit();
-
 	}
 
 }
