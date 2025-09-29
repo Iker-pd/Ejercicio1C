3aa6fdfac87c4a1da9cf9b85cf5cb98f590a813a
Iker-pd <iker.perez.dulce@opendeusto.es>
2025-09-29 09:24:53 +0200

    Creacion del proyecto

---
+++ Ejercicio1C/src/Ejercicio1C/VentanaSwingGit.java
@@ -0,0 +1,12 @@
+package Ejercicio1C;
+
+import javax.swing.JFrame;
+
+public class VentanaSwingGit extends JFrame {
+
+	public static void main(String[] args) {
+		new VentanaSwingGit();
+
+	}
+
+}
